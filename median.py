ae64f149228b98769323169ce72c0ce25103b5fd
fenn7 <afenn666@gmail.com>
2022-09-29 15:17:28 +0100

    commit

--- median.py
+++ median.py
@@ -1,6 +1,10 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from re import A
+from unittest import result
+
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -10,3 +14,19 @@ while True:
     else:
         break
 print(numbers)
+
+# sort the numbers in ascending order
+numbers.sort()
+
+# find the list length and then the median
+listLength = len(numbers)
+result = None
+
+if listLength %2 == 0 :
+    lowerNum = numbers[int((listLength / 2) - 1)]
+    higherNum = numbers[int(listLength / 2)]
+    result = (lowerNum + higherNum) / 2
+else:
+    result = numbers[int((listLength - 1) / 2)]
+
+print(result);
